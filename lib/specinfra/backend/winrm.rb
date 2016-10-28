0729451886f5e79c150d5f9fa42b6a10821667e2
Ethan Moore <github@proxyman.com>
2016-10-28 15:28:40 -0500

    update for winrm v2

--- lib/specinfra/backend/winrm.rb
+++ lib/specinfra/backend/winrm.rb
@@ -11,18 +11,18 @@ module Specinfra
         script = create_script(cmd)
         winrm = get_config(:winrm)
 
-        result = winrm.powershell(script)
-        stdout, stderr = [:stdout, :stderr].map do |s|
-          result[:data].select {|item| item.key? s}.map {|item| item[s]}.join
-        end
-        result[:exitcode] = 1 if result[:exitcode] == 0 and !stderr.empty?
+        result = winrm.run(script)
+	stdout = result.stdout
+	stderr = result.stderr
+	exitcode = result.exitcode
+	exitcode = 1 if result.exitcode == 0 and !stderr.empty?
 
         if @example
           @example.metadata[:command] = script
           @example.metadata[:stdout]  = stdout + stderr
         end
 
-        CommandResult.new :stdout => stdout, :stderr => stderr, :exit_status => result[:exitcode]
+        CommandResult.new :stdout => stdout, :stderr => stderr, :exit_status => exitcode
       end
     end
   end
